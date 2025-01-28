fab9f1c9b74b3968c026302b6e4c47250902fa54
DHW4J <dhwajagarwal195@gmail.com>
2025-01-28 23:29:31 +0530

    able to parse the csv file

--- main.go
+++ main.go
@@ -1,11 +1,50 @@
 package main
 
-	
-
-import "fmt"
-
-	
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
-    fmt.Println("hello world")
+	csvFilename := flag.String("csv", "problems.csv", "a csv file in format of 'question and answer'\n")
+	flag.Parse()
+
+	file, err := os.Open(*csvFilename)
+	if err != nil {
+		exit(fmt.Sprintf("Failed to open the CSV file: %s\n", *csvFilename))
+	}
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit("Failed to parse the CSV file.")
+	}
+	problems := parseLines(lines)
+	fmt.Println(problems)
+
+	for i, p := range problems {
+		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
+	}
+}
+
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+	for i, line := range lines {
+		ret[i] = problem{
+			q: line[0],
+			a: line[1],
+		}
+	}
+	return ret
+}
+
+type problem struct {
+	q string
+	a string
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
 }
