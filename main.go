31344615d173e0913f5fd6f918f1e7783e3f0974
DHW4J <dhwajagarwal195@gmail.com>
2025-02-01 22:47:34 +0530

    quiz game using go

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -23,9 +24,18 @@ func main() {
 	problems := parseLines(lines)
 	fmt.Println(problems)
 
+	correct := 0
 	for i, p := range problems {
 		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
+		var answer string
+		fmt.Scanf("%s\n", &answer)
+		if answer == p.a {
+			correct++
+
+		}
+
 	}
+	fmt.Printf("You scored %d out of %d\n", correct, len(problems))
 }
 
 func parseLines(lines [][]string) []problem {
@@ -33,7 +43,7 @@ func parseLines(lines [][]string) []problem {
 	for i, line := range lines {
 		ret[i] = problem{
 			q: line[0],
-			a: line[1],
+			a: strings.TrimSpace(line[1]),
 		}
 	}
 	return ret
